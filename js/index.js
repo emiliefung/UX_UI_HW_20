0a6fab6241c67376769d9705325aa6b28882f66d
Emilie Fung <emilie.fung12@gmail.com>
2022-02-26 16:59:58 -0500

    update

---
+++ js/index.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    $(".hover1").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "#3c3c3c");
+    });
+
+    $(".hover2").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "#3c3c3c");
+    });
+
+    $(".hover3").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "#3c3c3c");
+    });
+
+    $(".hover4").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "#3c3c3c");
+    });
+
+    $(".hover5").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "white");
+    });
+
+    $(".hover6").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "white");
+    });
+
+    $(".hover7").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "white");
+    });
+
+    $(".hover8").hover(function() {
+        $(this).css("color", "#ffbf34");
+    },
+    function(){
+        $(this).css("color", "white");
+    });
+
+
+
+    
+});
+
